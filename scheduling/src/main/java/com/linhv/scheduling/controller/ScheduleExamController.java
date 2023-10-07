67d8007d9327f2d65a8a2778dd8f5d0935743445
Rinv <prodige112002@gmail.com>
2023-10-07 23:37:51 +0700

    [Linh] add controllers

---
+++ scheduling/src/main/java/com/linhv/scheduling/controller/ScheduleExamController.java
@@ -0,0 +1,9 @@
+package com.linhv.scheduling.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/exams")
+public class ScheduleExamController {
+}
