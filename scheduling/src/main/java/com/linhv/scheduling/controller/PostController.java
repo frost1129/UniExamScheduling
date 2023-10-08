dfa36e6cce7459ccf636238eae69d9e5f97c5d0a
Rinv <prodige112002@gmail.com>
2023-10-09 01:09:52 +0700

    [Linh] post controller

--- scheduling/src/main/java/com/linhv/scheduling/controller/PostController.java
+++ scheduling/src/main/java/com/linhv/scheduling/controller/PostController.java
@@ -1,9 +1,47 @@
 package com.linhv.scheduling.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.linhv.scheduling.model.Post;
+import com.linhv.scheduling.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/posts")
 public class PostController {
+
+    @Autowired
+    private PostService postService;
+
+    @GetMapping
+    public ResponseEntity<List<Post>> getPosts(@RequestParam Map<String, String> params) {
+        return new ResponseEntity<>(this.postService.getAll(params), HttpStatus.OK);
+    }
+
+    @GetMapping("/top-5/")
+    public ResponseEntity<List<Post>> getTop5Post() {
+        return new ResponseEntity<>(postService.getTop5(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{postId}")
+    public ResponseEntity<Post> getPostById(@PathVariable(value = "postId") String id) {
+        return new ResponseEntity<>(this.postService.getById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/create", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<String> createPost(@ModelAttribute Post post) {
+        this.postService.newPost(post);
+        return new ResponseEntity<>("created", HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/delete/{postId}")
+    public ResponseEntity<String> deletePost(@PathVariable(value = "postId") String id) {
+        this.postService.deletePost(id);
+        return new ResponseEntity<>("deleted", HttpStatus.NO_CONTENT);
+    }
 }
--- scheduling/src/main/java/com/linhv/scheduling/service/impl/PostServiceImpl.java
+++ scheduling/src/main/java/com/linhv/scheduling/service/impl/PostServiceImpl.java
@@ -1,20 +1,33 @@
 package com.linhv.scheduling.service.impl;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import com.github.slugify.Slugify;
 import com.linhv.scheduling.model.Post;
 import com.linhv.scheduling.repository.PostRepository;
 import com.linhv.scheduling.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Service
 public class PostServiceImpl implements PostService {
 
     @Autowired
     PostRepository postRepo;
+
+    @Autowired
+    Cloudinary cloudinary;
+
+    private Slugify slg = Slugify.builder().build();
 
     @Override
     public Post getById(String id) {
@@ -43,12 +56,43 @@ public class PostServiceImpl implements PostService {
 
     @Override
     public Post newPost(Post post) {
+//        slugify post id
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd-HHmmss");
+        String formattedDate = dateFormat.format(new Date());
+        post.setId(slg.slugify(post.getTitle()) + "-" + formattedDate);
+
+//        set post data
+        post.setUpdatedDate(new Date());
+        uploadImage(post, post);
+
         return postRepo.save(post);
     }
 
     @Override
     public Post updatePost(String id, Post post) {
-        return null;
+        try {
+            Post curPost = this.getById(id);
+
+            curPost.setContent(post.getContent());
+            curPost.setUpdatedDate(new Date());
+            curPost.setTitle(post.getTitle());
+            uploadImage(post, curPost);
+
+            return this.postRepo.save(curPost);
+        } catch (NoSuchElementException ex) {
+            return null;
+        }
+    }
+
+    private void uploadImage(Post post, Post toUpdatePost) {
+        if (!post.getImageFile().isEmpty()) {
+            try {
+                Map res = this.cloudinary.uploader().upload(post.getImageFile().getBytes(), ObjectUtils.asMap("resource_type", "auto"));
+                toUpdatePost.setImage(res.get("secure_url").toString());
+            } catch (IOException ex) {
+                Logger.getLogger(PostServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
     }
 
     @Override
